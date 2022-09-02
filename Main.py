e93262ceb3d7142d02e03c498f4a026b3ab171a0
URK21CS1046 <109952564+URK21CS1046@users.noreply.github.com>
2022-09-02 16:56:37 +0530

    initial commit

--- Main.py
+++ Main.py
@@ -8,26 +8,25 @@ class Node:
 class DoublyCircularLinkedList:
     def __init__(self):
         self.head = None
-        self.tail = none
+        self.tail = None
         self.count = 0
-
+        
     def add_at_tail(self, data) -> bool:
-        # Write code here
         node = Node(data)
-        if self.count >0:
-            node.previous = node
+        if self.count > 0:
+            node.previous = self.tail
             self.tail.next = node
             self.head.previous = node
             node.next = self.head
         else:
             self.head = node
-            self.count += 1
-            return true
+        self.tail = node
+        self.count += 1
+        return True
 
-    def add_at_head(self, data) -> bool:
-        # Write code here
+    def add_at_head(self, data) -> bool:   
         node = Node(data)
-        if self.count>0:
+        if self.count > 0:
             node.next = self.head
             node.previous = self.tail
             self.head.previous = node
@@ -36,64 +35,70 @@ class DoublyCircularLinkedList:
             self.tail = node
         self.head = node
         self.count += 1
-        return true
-
+        return True
+    
     def add_at_index(self, index, data) -> bool:
-        # Write code here
-        if index >= self.count:
-            return false
-        elif index == 0:
+        if index < 0 or index >= self.count:
+            return False
+        if index == 0:
             return self.add_at_head(data)
-        elif index == self.count:
-           return self.add_at_tail(data)
-    node = Node(data)
-    alex_node = self.head
-    for i in range(index):
-        alex_node = alex_node.next
-    node.previous = alex_node.previous
-    node.next = alex_node
-    alex_node.previous.next = node
-    alex_node.previous = node
-    self.count += 1
-    return true
+        if index == self.count:
+            return self.add_at_tail(data)
+        
+        node = Node(data)
+        alex_node = self.head
+        for i in range(index):
+            alex_node = alex_node.next
+        node.previous = node.previous
+        node.next = node
+        alex_node.previous.next = node
+        alex_node.previous = node
+        self.count += 1
+        return True        
 
     def get(self, index) -> int:
-        # Write code here
-        if index >= self.count:
+        if index < 0 or index >= self.count:
             return -1
-        aalex_node = self.head
+        alex_node = self.head
         for i in range(index):
             alex_node = alex_node.next
         return alex_node.data
-
+       
     def delete_at_index(self, index) -> bool:
-        # Write code here
-        if index >= self.count:
-            return false
+        if index < 0 or index >= self.count:
+            return False
         if index == 0:
             temp = self.head
             temp.next.previous = self.tail
             self.tail.next = temp.next
             self.head = temp.next
             self.count -= 1
-            return true
-        if index == self.count-1:
+            return True
+        if index == self.count - 1:
             temp = self.tail
             self.head.previous = temp.previous
             temp.previous.next = self.head
             self.tail = temp.previous
             self.count -= 1
-            return true
+            return True
+       
+        alex_node = self.head
+        for j in range(index):
+            alex_node = alex_node.next
+        alex_node.previous.next = alex_node.next
+        alex_node.next.previous = alex_node.previous
+        self.count -= 1
+        return True
+        
 
     def get_previous_next(self, index) -> list:
-        # Write code here
-        if index >= self.count:
+        if index < 0 or index >= self.count:
             return -1
         alex_node = self.head
-        for i in range(index):
+        for ind in range(index):
             alex_node = alex_node.next
-        returnn [alex_node.previous.data, alex_node.next.data]
-
+        return [alex_node.previous.data, alex_node.next.data]
+        
 
 # Do not change the following code
 operations = []
